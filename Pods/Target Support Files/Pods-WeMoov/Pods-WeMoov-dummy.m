d6e1c45ddc196e6151cb7d76b7b09625631693c8
Victor-Dev77 <deyanovitch77580@gmail.com>
2019-12-19 15:17:20 +0100

    Link Firebase to App. Pull and Open Wemoov.workspace & Pod install

---
+++ Pods/Target Support Files/Pods-WeMoov/Pods-WeMoov-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeMoov : NSObject
+@end
+@implementation PodsDummy_Pods_WeMoov
+@end
--- WeMoov/AppDelegate.swift
+++ WeMoov/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.rootViewController = UINavigationController(rootViewController: LoginViewController())
         window.makeKeyAndVisible()
         self.window = window
+        FirebaseApp.configure()
         return true
     }
 
