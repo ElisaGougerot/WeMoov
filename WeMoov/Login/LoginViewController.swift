970aaab600d0fc1009b5723176a8849005787a15
Taj <singh_taj@hotmail.fr>
2019-12-19 01:41:26 +0100

    Suppression du storyboard

---
+++ WeMoov/Login/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  WeMoov
+//
+//  Created by Taj Singh on 19/12/2019.
+//  Copyright © 2019 Elisa Gougerot. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
